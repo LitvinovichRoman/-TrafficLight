8f604504b2672a2cd5ebf55ea9896b58b6f05304
LitvinovichRoman <127106050+LitvinovichRoman@users.noreply.github.com>
2023-12-09 11:14:36 +0300

    project created

---
+++ TrafficLight/TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Roman Litvinovich on 09/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
